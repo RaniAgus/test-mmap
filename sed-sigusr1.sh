5776d17fae90f547b926de6e8681dbfa6a8ac246
Agustin Ranieri <aguseranieri@gmail.com>
2021-05-15 19:56:04 -0300

    mejoro el test

--- sed-sigusr1.sh
+++ sed-sigusr1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROCESS_ID=$(pgrep test-mmap)
 echo "Antes: $(<blocks.txt)"
-sed -i 's/AAA/BBB/' blocks.txt
+sed -i 's/./#/g' blocks.txt
 echo "Despues: $(<blocks.txt)"
 kill -SIGUSR1 $PROCESS_ID
 echo "kill -SIGUSR1 $PROCESS_ID"
--- src/prueba.c
+++ src/prueba.c
@@ -21,6 +21,12 @@
 #include <commons/memory.h>
 #include <commons/temporal.h>
 
+#define FILE_SIZE 25
+#define FILE_PATH "blocks.txt"
+
+char* mapeado;
+char buffer[FILE_SIZE + 1];
+
 void handler(int signum) {
 	char* time = temporal_get_string_time("%H:%M:%S:%MS");
 	printf("\n\n================================================================================\n"
@@ -29,36 +35,66 @@ void handler(int signum) {
 	free(time);
 }
 
+char* read_file() {
+	FILE *pipe;
+
+	if((pipe = popen("cat " FILE_PATH, "r")) == NULL) {
+		perror("error al intentar abrir el archivo");
+	}
+
+	for(int i = 0; i < FILE_SIZE; i++) {
+		buffer[i] = fgetc(pipe);
+	}
+	buffer[FILE_SIZE] = '\0';
+
+	pclose(pipe);
+	return buffer;
+}
+
+void print_map_and_file(char* str) {
+	printf("Map %s:\n", str);
+	mem_hexdump(mapeado, FILE_SIZE);
+	printf("\nFile %s:\n", str);
+	mem_hexdump(read_file(), FILE_SIZE);
+	printf("--------------------------------------------------------------------------------\n");
+}
+
 int main(void) {
 	printf("\n\n================================================================================\n"
 			"My PID: %d\n\n"
 			, getpid());
 
-	int fd = open("blocks.txt", O_RDWR | O_CREAT, (mode_t) 0777);
+	int fd = open(FILE_PATH, O_RDWR | O_CREAT, (mode_t) 0777);
 	if(fd == -1){
-		perror("error abriendo blocks.txt");
+		perror("error al intentar abrir el archivo");
 		exit(-1);
 	}
-	fallocate(fd, 0, 0, 25);
+	fallocate(fd, 0, 0, FILE_SIZE);
 
-	char* mapeado = mmap(NULL, 25, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
-	memset(mapeado, 'A', 25);
-	mem_hexdump(mapeado, 25);
+	mapeado = mmap(NULL, 25, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+	memset(mapeado, 'A' + rand() % 25, FILE_SIZE);
+	msync(mapeado, FILE_SIZE, 0);
+
+	print_map_and_file("at start");
 
 	signal(SIGUSR1, handler);
 
 	while(1) {
 		sleep(1000000000);
 
-		printf("\nBefore msync():\n\n");
-		mem_hexdump(mapeado, 25);
-		msync(mapeado, 25, 0);
-		printf("\nAfter msync():\n\n");
-		mem_hexdump(mapeado, 25);
+		print_map_and_file("before msync()");
+	
+		msync(mapeado, FILE_SIZE, 0);
 
-		memset(mapeado, 'A', 25);
-		msync(mapeado, 25, 0);
-		printf("\nFile restarted\n\n");
+		print_map_and_file("after msync(), before memset()");
+
+		memset(mapeado, 'A' + rand() % 25, FILE_SIZE);
+
+		print_map_and_file("after memset(), before msync()");
+
+		msync(mapeado, FILE_SIZE, 0);
+
+		print_map_and_file("after msync()");
 	}
 
 
